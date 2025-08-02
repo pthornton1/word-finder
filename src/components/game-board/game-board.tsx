9e1655defcb5a09887fd4b14f5d17512827dea95
Philip Thornton <philipthornton@Philips-MacBook-Air-2.local>
2025-08-02 21:47:01 +0100

    add word checking

--- src/components/game-board/game-board.tsx
+++ src/components/game-board/game-board.tsx
@@ -1,14 +1,111 @@
+"use client";
+
 import Tile from "@/components/game-board/tile/tile";
+import { useState } from "react";
 
 const words = ["apple", "score", "marry", "toned", "loved"];
+const solutionArray = [
+  [
+    { x: 0, y: 0 }, // a
+    { x: 0, y: 1 }, // p
+    { x: 0, y: 2 }, // p
+    { x: 0, y: 3 }, // l
+    { x: 0, y: 4 }, // e
+  ],
+  [
+    { x: 1, y: 0 }, // s
+    { x: 1, y: 1 }, // c
+    { x: 1, y: 2 }, // o
+    { x: 1, y: 3 }, // r
+    { x: 1, y: 4 }, // e
+  ],
+  [
+    { x: 2, y: 0 }, // m
+    { x: 2, y: 1 }, // a
+    { x: 2, y: 2 }, // r
+    { x: 2, y: 3 }, // r
+    { x: 2, y: 4 }, // y
+  ],
+  [
+    { x: 3, y: 0 }, // t
+    { x: 3, y: 1 }, // o
+    { x: 3, y: 2 }, // n
+    { x: 3, y: 3 }, // e
+    { x: 3, y: 4 }, // d
+  ],
+  [
+    { x: 4, y: 0 }, // l
+    { x: 4, y: 1 }, // o
+    { x: 4, y: 2 }, // v
+    { x: 4, y: 3 }, // e
+    { x: 4, y: 4 }, // d
+  ],
+];
+const totalWords = 5; // Total number of words to find
+
+type tilesType = {
+  x: number;
+  y: number;
+};
+
+const checkIfCompleted = (selectedTiles: tilesType[]) => {
+  if (selectedTiles.length !== 5) return false;
+
+  return solutionArray.some(
+    (solution) =>
+      solution.length === selectedTiles.length &&
+      selectedTiles.every((tile) =>
+        solution.some(
+          (solutionTile) =>
+            solutionTile.x === tile.x && solutionTile.y === tile.y
+        )
+      )
+  );
+};
 
 export default function GameBoard() {
+  const [selectedTiles, setSelectedTiles] = useState<tilesType[]>([]);
+  const [completedTiles, setCompletedTiles] = useState<tilesType[]>([]);
+  const [foundWords, setFoundWords] = useState<number>(0);
+
+  const onTileSelect = ({ x, y }: { x: number; y: number }) => {
+    const currentSelection = [...selectedTiles];
+    if (currentSelection.some((tile) => tile.x === x && tile.y === y)) {
+      const index = currentSelection.findIndex(
+        (tile) => tile.x === x && tile.y === y
+      );
+      currentSelection.splice(index, 1);
+    } else {
+      currentSelection.push({ x, y });
+    }
+    setSelectedTiles(currentSelection);
+    if (checkIfCompleted(currentSelection)) {
+      setSelectedTiles([]);
+      setCompletedTiles([...currentSelection, ...completedTiles]);
+      setFoundWords(foundWords + 1);
+      if (foundWords + 1 === totalWords) {
+        alert("All words found! Great job!");
+      }
+    }
+  };
+
   return (
     <div className="p-4">
       {words.map((word, index) => (
         <div key={index} className="flex space-x-4 mb-4">
           {word.split("").map((letter, letterIndex) => (
-            <Tile key={letterIndex} letter={letter} completed={false} />
+            <Tile
+              key={letterIndex}
+              letter={letter}
+              completed={completedTiles.some(
+                (tile) => tile.x === index && tile.y === letterIndex
+              )}
+              selected={selectedTiles.some(
+                (tile) => tile.x === index && tile.y === letterIndex
+              )}
+              position={{ x: index, y: letterIndex }}
+              onTileSelect={onTileSelect}
+            />
           ))}
         </div>
       ))}
--- src/components/game-board/tile/tile.tsx
+++ src/components/game-board/tile/tile.tsx
@@ -1,29 +1,36 @@
-"use client";
-import { useState } from "react";
-
 export default function Tile({
   letter,
-  completed = false,
+  completed,
+  selected,
+  onTileSelect,
+  position,
 }: {
   letter: string;
   completed: boolean;
+  position: { x: number; y: number };
+  onTileSelect: (tile: { x: number; y: number }) => void;
+  selected: boolean;
 }) {
-  const [isSelected, setIsSelected] = useState(false);
+  const handleTileClick = () => {
+    onTileSelect(position);
+  };
+
   if (!completed)
     return (
       <button
-        onClick={() => setIsSelected(!isSelected)}
+        onClick={() => handleTileClick()}
         className={`ring-2 rounded-lg p-6 flex items-center justify-center ${
-          isSelected ? "bg-green-300" : "bg-gray-200"
+          selected ? "bg-green-300" : "bg-gray-200"
         } transition-colors duration-300 hover:bg-blue-200 focus:outline-none hover:cursor-pointer`}
       >
         <span className="text-lg font-bold">{letter.toUpperCase()}</span>
       </button>
     );
-  <button
-    onClick={() => setIsSelected(!isSelected)}
-    className={`ring-2 rounded-lg p-6 flex items-center justify-center hover:cursor-disabled`}
-  >
-    <span className="text-lg font-bold">{letter.toUpperCase()}</span>
-  </button>;
+  return (
+    <button
+      className={`ring-2 rounded-lg p-6 flex items-center justify-center hover:cursor-disabled bg-green-500`}
+    >
+      <span className="text-lg font-bold">{letter.toUpperCase()}</span>
+    </button>
+  );
 }
