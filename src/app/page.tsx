b9c50a6d323b463f47c256207f0e5b3164412186
Philip Thornton <philipthornton@Mac.Home>
2025-09-27 15:05:38 +0100

    add styling change for welcome message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import GameBoard from "@/components/game-board/game-board";
 export default function Home() {
   return (
     <div className="flex flex-col items-center">
-      <h1 className="text-4xl font-bold">Welcome to Word Finder</h1>
+      <h1 className="text-3xl font-bold px-5">Welcome to Word Finder</h1>
       <p className="mt-4 text-lg">
         Complete the game by finding all the words!
       </p>
