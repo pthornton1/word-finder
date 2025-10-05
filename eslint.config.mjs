def0165baeceaacf763c2907b15ef181ca0755e6
Phil Thornton <philipthornton198@gmail.com>
2025-10-05 16:14:55 +0100

    add girhub workflow actions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "public/**",
+      "coverage/**",
+    ],
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
